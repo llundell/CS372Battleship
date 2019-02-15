f08c859edb948c8b08638e6b97c76250528c8691
llundell <llundell@ymail.com>
2019-02-14 17:30:41 -0900

    Revert "Revert "add new files for catch2 testing""
    This reverts commit 40a3d3df9db76ac4d069658d3d91f21b12cdce81.

---
+++ cmake-build-debug/CMakeFiles/cs372.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/factorial.cpp" "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/cmake-build-debug/CMakeFiles/cs372.dir/factorial.cpp.o"
+  "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/factorial_tests.cpp" "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/cmake-build-debug/CMakeFiles/cs372.dir/factorial_tests.cpp.o"
+  "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/string_calc_tests.cpp" "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/cmake-build-debug/CMakeFiles/cs372.dir/string_calc_tests.cpp.o"
+  "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/stringcalc.cpp" "/Users/laura/Documents/UAF/CS372SoftwareConstruction/cs372/cmake-build-debug/CMakeFiles/cs372.dir/stringcalc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
